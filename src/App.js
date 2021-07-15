1ec88591672bf097e8ad54883d3ae3ab2db49e3c
Давид Шакая <66774063+david-shakaya@users.noreply.github.com>
2021-07-15 22:05:39 +0300

    added structure and container

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import "./App.css";
+import Container from "./components/Container/Container";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Container>
+          <p>Тут у нас хедер</p>
+        </Container>
+      </header>
+    </div>
+  );
+}
+
+export default App;
